afe638340861270ebda3d85574bc64c9dd1216e2
Aleksei <doj20@mail.ru>
2022-05-18 21:25:12 +0400

    FillArray method has been changed

--- Program.cs
+++ Program.cs
@@ -26,16 +26,17 @@ else
     Console.WriteLine(randomArray[indexRow, indexCol]);
 }
 
-void FillArray(double[,] array, int numberRow, int numberCol, int lowerLim, int upperLim, int precision)
+double [,] FillArray(double[,] array, int numberRow, int numberCol, int lowerLim, int upperLim, int precision)
 {
-    for (int i = 0; i < randomArray.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < randomArray.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             double randomNum = new Random().NextDouble() * (upperLim - lowerLim);
-            randomArray[i, j] = Math.Round(randomNum, precision);
+            array[i, j] = Math.Round(randomNum, precision);
         }
     }
+    return array;
 }
 
 void PrintArray(double[,] array)
